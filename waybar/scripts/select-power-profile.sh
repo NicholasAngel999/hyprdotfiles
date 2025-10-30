812aa61c274cf9d23fea428b795389148d08d1f4
NicholasAngel999 <colesumner05@gmail.com>
2025-10-30 10:13:31 +0000

    backup

--- waybar/scripts/select-power-profile.sh
+++ waybar/scripts/select-power-profile.sh
@@ -5,7 +5,7 @@ options="Performance\nBalanced\nPower Saver"
 
 # Show the menu using wofi in dmenu mode and capture the selection
 # Point -S to a file that doesn't exist
-selected=$(echo -e "$options" | wofi --dmenu --prompt "Select Power Profile:" -S ~/.config/wofi/style-powermenu.css)
+selected=$(echo -e "$options" | wofi --dmenu --prompt "Select Power Profile:" --style ~/.config/wofi/style-powermenu.css)
 
 # Execute the command based on the selection
 case $selected in
@@ -24,8 +24,8 @@ case $selected in
 esac
 
 # Optional: Send a notification to confirm the change (requires notify-send)
-# if [ -n "$selected" ]; then
-#   notify-send "Power Profile Set" "$selected"
-# fi
+if [ -n "$selected" ]; then
+  notify-send "Power Profile Set" "$selected"
+fi
 
 exit 0
